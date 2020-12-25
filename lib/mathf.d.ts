ad30ca395f55449ce6170b7e7892325e969fc0d0
Hoyoung Jung <honux77@gmail.com>
2020-12-25 22:34:36 +0900

    Update 0.1.2

--- lib/mathf.d.ts
+++ lib/mathf.d.ts
@@ -1,6 +1,7 @@
 declare class Mathf {
     static PI: number;
     static E: number;
+    static TAU: number;
     static isOdd: (num: number) => boolean;
     static isEven: (num: number) => boolean;
 }
